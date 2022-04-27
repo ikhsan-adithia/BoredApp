8ae74b1532482eae27a0bd7e3684c3e3c7f0ba92
ikhsan <ikhsanadithia24@gmail.com>
2022-04-27 15:56:06 +0700

    Init commit

---
+++ app/src/main/java/com/bored/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.bored.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bored.app.ui.theme.BoredAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BoredAppTheme {
+            }
+        }
+    }
+}
+
